e73a707c43b2586fa4a69ea1f8e333dbc8747f5c
pawel200020 <38167430+pawel200020@users.noreply.github.com>
2021-02-24 02:31:26 +0100

    rev with dict 1.0 untested

--- main.py
+++ main.py
@@ -5,15 +5,26 @@ class Data:
 
 if __name__ == '__main__':
     quantity = int(input())
-    data = Data
+    dict = {}
     for i in range(quantity):
         a = input()
-        if a not in data.words:
-            data.words.append(a)
-            data.quantity.append(1)
+        if a in dict:
+            dict[a] += 1
         else:
-            c = data.words.index(a)
-            data.quantity[c] += 1
-    print(len(data.words))
-    for i in data.quantity:
-        print(i, end=" ")
+            dict[a] = 1
+    print(len(dict))
+    for i in dict:
+        print(dict[i],end=" ")
+
+    # data = Data
+    #
+    #     a = input()
+    #     if a not in data.words:
+    #         data.words.append(a)
+    #         data.quantity.append(1)
+    #     else:
+    #         c = data.words.index(a)
+    #         data.quantity[c] += 1
+    # print(len(data.words))
+    # for i in data.quantity:
+    #     print(i, end=" ")
