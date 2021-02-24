871288871775ad1c8622f10223aa559b08288af2
pawel200020 <38167430+pawel200020@users.noreply.github.com>
2021-02-24 02:32:57 +0100

    final rev, fully working

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-class Data:
-    words = list()
-    quantity = list()
-
-
 if __name__ == '__main__':
     quantity = int(input())
     dict = {}
@@ -14,17 +9,4 @@ if __name__ == '__main__':
             dict[a] = 1
     print(len(dict))
     for i in dict:
-        print(dict[i],end=" ")
-
-    # data = Data
-    #
-    #     a = input()
-    #     if a not in data.words:
-    #         data.words.append(a)
-    #         data.quantity.append(1)
-    #     else:
-    #         c = data.words.index(a)
-    #         data.quantity[c] += 1
-    # print(len(data.words))
-    # for i in data.quantity:
-    #     print(i, end=" ")
+        print(dict[i], end=" ")
